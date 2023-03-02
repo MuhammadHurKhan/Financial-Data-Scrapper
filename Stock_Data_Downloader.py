c15299bdda7a59d0b7c6ec5c03b040d943d5aa8c
Muhammad Hur Khan <122796904+MuhammadHurKhan@users.noreply.github.com>
2023-03-02 17:56:56 +0500

    Update Stock_Data_Downloader.py

--- Stock_Data_Downloader.py
+++ Stock_Data_Downloader.py
@@ -6,8 +6,6 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-import base64
-import plotly.graph_objs as go
 
 # Set page title and favicon
 st.set_page_config(page_title="Stock Data Downloader", page_icon=":money_with_wings:")
@@ -16,7 +14,7 @@ st.set_page_config(page_title="Stock Data Downloader", page_icon=":money_with_wi
 st.markdown("""
 <style>
 h1, h2, h3 {
-    color: #000080.;
+    color: #252525;
 }
 .btn-primary {
     background-color: #3366FF;
@@ -42,22 +40,26 @@ if st.button("Download Data as CSV", key="download"):
     filename = f"{ticker}_{start_date}_{end_date}.csv"
     data.to_csv(filename)
     st.success(f"Data downloaded to {filename}.")
-    
-    # Create a download link for the CSV file
-    with open(filename, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data).decode()
-        href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download CSV</a>'
-        st.markdown(href, unsafe_allow_html=True)
 
 # Display the data in a table
 if st.button("Show Data", key="show"):
     data = yf.download(ticker, start=start_date, end=end_date)
     st.write(data)
-    
-   # Display a line chart of the closing price
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data.index, y=data['Close'], name="Closing Price"))
-    fig.update_layout(title=f"{ticker} Closing Price", xaxis_title="Date", yaxis_title="Price")
-    st.plotly_chart(fig)
 
+# Add your information to the app
+st.markdown("""
+## About Me
+My name is Muhammad Hur Khan, and I'm on a mission to help businesses achieve their goals through data analysis and financial modeling. With a keen eye for business process analysis and optimization, I'm dedicated to finding ways to drive your business growth and take you to the next level.
+
+As an experienced operational assistant and accounting analyst, I've helped companies identify new business opportunities and increase their revenue, while streamlining their processes and improving performance. I'm proficient in Power BI and other CRM tools, and I'm always ready to dive into your data to uncover insights that can help you make informed decisions and drive your business forward.
+
+With a degree in Computational Finance from NED University of Engineering and Technology and certifications in EFSET English and Power BI, I'm a skilled communicator and a creative problem solver. I know how to analyze financial data and make it accessible and understandable to non-experts, so you can make informed decisions and achieve your business goals.
+
+If you're looking for a data analyst who can help you optimize your business, then let's connect! I'm eager to hear about your business and your goals, and I'm ready to bring my expertise to help you succeed.
+""")
+
+# Add closing price graph
+if st.button("Show Closing Price Graph", key="graph"):
+    data = yf.download(ticker, start=start_date, end=end_date)
+    fig = data.Close.plot()
+    st.write(fig)
