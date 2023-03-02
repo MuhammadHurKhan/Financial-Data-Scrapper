cd1bf707961542e46b596c51711591e822f98b57
Muhammad Hur Khan <122796904+MuhammadHurKhan@users.noreply.github.com>
2023-03-02 16:47:37 +0500

    Update Stock_Data_Downloader.py

--- Stock_Data_Downloader.py
+++ Stock_Data_Downloader.py
@@ -9,32 +9,38 @@ import yfinance as yf
 import streamlit as st
 import base64
 
-# Create a Streamlit application
-st.title("Stock Data Downloader")
-
-# Get the ticker symbol and time period from the user
-ticker = st.text_input("Enter the ticker symbol for the desired stock or index (e.g., AAPL, ^GSPC):")
-start_date = st.date_input("Enter the start date for the desired time period:")
-end_date = st.date_input("Enter the end date for the desired time period:")
-
-# Convert the start and end dates to strings
-start_date_str = start_date.strftime("%Y-%m-%d")
-end_date_str = end_date.strftime("%Y-%m-%d")
-
-try:
-    # Retrieve the data for the specified stock or index and time period
+# Define function to download stock data
+@st.cache
+def download_data(ticker, start_date, end_date):
+    start_date_str = start_date.strftime("%Y-%m-%d")
+    end_date_str = end_date.strftime("%Y-%m-%d")
     data = yf.download(ticker, start=start_date_str, end=end_date_str)
+    return data
 
-    # Remove the timezone information from the datetime index
-    data.index = data.index.tz_localize(None)
+# Define main function for app
+def main():
+    # Set page title and icon
+    st.set_page_config(page_title='Stock Data Downloader', page_icon=':chart_with_upwards_trend:')
+    
+    # Define sidebar inputs
+    st.sidebar.title('Stock Data Downloader')
+    ticker = st.sidebar.text_input('Enter Ticker Symbol (e.g. AAPL)')
+    start_date = st.sidebar.date_input('Enter Start Date', value=pd.to_datetime('2010-01-01'))
+    end_date = st.sidebar.date_input('Enter End Date', value=pd.to_datetime('today'))
+    
+    # Define welcome message
+    st.write('Welcome to the Stock Data Downloader! Please wait while we retrieve your data...')
+    
+    # Download data and show table
+    try:
+        data = download_data(ticker, start_date, end_date)
+        st.write(data)
+    except Exception as e:
+        st.write('An error occurred while retrieving the data. Please try again later.')
+        st.write('Error Details:', e)
 
-    # Show the data to the user
-    st.write(data)
-except Exception as e:
-    st.error("An error occurred while retrieving the data. Please try again later.")
-# Display the data in a table
-st.write(data)
-
+if __name__ == '__main__':
+    main()
 # Allow the user to download the data in a CSV file format
 csv = data.to_csv(index=False)
 b64 = base64.b64encode(csv.encode()).decode()
